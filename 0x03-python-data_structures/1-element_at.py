4dc9073663917cc9467edec29faa17f3d172347c
Caroll1889 <diahancaroll@hotmail.com>
2019-09-17 17:13:27 +0000

    verified the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,10 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+
+    if my_list == []
+        return None
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
