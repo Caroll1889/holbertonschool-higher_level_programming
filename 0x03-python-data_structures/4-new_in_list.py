717aeb4a7aaf607f7c14c63e4c4973de26dbe38e
Caroll1889 <diahancaroll@hotmail.com>
2019-09-17 21:01:05 +0000

    new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+
+    if my_list is None:
+        return my_list
+
+    newlist = my_list.copy()
+
+    if idx < 0 or idx >= len(my_list):
+        return newlist
+
+    newlist[idx] = element
+    return newlist
