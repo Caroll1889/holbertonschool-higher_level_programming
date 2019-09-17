d4b74bddcb2088246ca89758450beb13f2c50b13
Caroll1889 <diahancaroll@hotmail.com>
2019-09-17 20:42:48 +0000

    put an else

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,5 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         print("", end="")
 
-    for i in my_list[::-1]:
-        print("{:d}".format(i))
+    else:
+
+        for i in my_list[::-1]:
+            print("{:d}".format(i))
