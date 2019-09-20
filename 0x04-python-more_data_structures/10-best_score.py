7dc0f77728dc89a9a5ec772fdf8166762adc8dbc
Caroll1889 <diahancaroll@hotmail.com>
2019-09-20 23:04:18 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+
+    if a_dictionary is None:
+        return None
+
+    else:
+        keymax = max(a_dictionary.keys())
+
+    return keymax
