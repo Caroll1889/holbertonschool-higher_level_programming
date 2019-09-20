674121db9d57853d46b7679565de8b388358ec05
Caroll1889 <diahancaroll@hotmail.com>
2019-09-20 23:14:53 +0000

    Rm return & var

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
 
     else:
-        keymax = max(a_dictionary.keys())
-
-    return keymax
+        return max(a_dictionary.keys())
