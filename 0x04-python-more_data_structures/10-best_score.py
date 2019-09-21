f14b867be7b60f7d2834858b1c601d461e40bfc6
Caroll1889 <diahancaroll@hotmail.com>
2019-09-21 00:41:09 +0000

    changed all the code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
 
     if not a_dictionary:
         return None
 
-    else:
-        return max(a_dictionary.keys())
+    keymax = max(a_dictionary.values())
+
+    for i in a_dictionary.keys():
+        if a_dictionary[i] == keymax:
+            return i
