456a00489516c042fbf804248633bf13de48760f
Caroll1889 <diahancaroll@hotmail.com>
2019-09-30 22:56:30 +0000

    advanced

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+
+    try:
+        print("{:d}".format(value))
+
+    except Exception as ex:
+        sys.stderr.write("Exception: "  + str(ex) + "\n")
+        return False
+
+    else:
+        return True
