41fd8e7018f5d235eb21653b9abb47706c4a3be0
Caroll1889 <diahancaroll@hotmail.com>
2019-09-30 22:59:11 +0000

    fixed pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,13 +2,14 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
 
     try:
         print("{:d}".format(value))
 
     except Exception as ex:
-        sys.stderr.write("Exception: "  + str(ex) + "\n")
+        sys.stderr.write("Exception: " + str(ex) + "\n")
         return False
 
     else:
