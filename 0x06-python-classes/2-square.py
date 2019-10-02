cc8a5b2762c615ba1ed2766acea2c713f8dddd63
Caroll1889 <diahancaroll@hotmail.com>
2019-10-02 00:23:52 +0000

    put a space in line 9

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
 
     def __init__(self, size=0):
 
-        if type(size) != int: # chequear si es un entero o no
+        if type(size) != int:  # chequear si es un entero o no
             raise TypeError("size must be an integer")
 
         if size < 0:
