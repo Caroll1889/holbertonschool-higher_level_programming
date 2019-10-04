c1129344e92d61ccc3f8d2263e24d91f91b36182
Caroll1889 <diahancaroll@hotmail.com>
2019-10-04 23:11:29 +0000

    fixed pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-
+    """Function that prints a name
+    first_name and last_name must be strings
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
 
