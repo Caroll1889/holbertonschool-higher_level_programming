81ed4428626739baf0cd54fcfa2e98c4c70d8f3d
Caroll1889 <diahancaroll@hotmail.com>
2019-10-05 18:55:14 +0000

    add module comment and fixed pycodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+""" module "matrix"
+Module that divides all the elements of a matrix"""
+
 
 def matrix_divided(matrix, div):
     """Function that divides all elements of a matrix
@@ -7,15 +10,16 @@ def matrix_divided(matrix, div):
     div is a integer and greater or equal than 0
     the function must return a new matrix
     """
+    typeError = "matrix must be a matrix (list of lists) of integers/floats"
 
     new_matrix = []
 
     for i in range(len(matrix)):
         if type(matrix[i]) != list:
-            raise TypeError("matrix must be a list of list")
+            raise TypeError("matrix must be a list of lists")
         for j in range(len(matrix[i])):
             if type(matrix[i][j]) not in(int, float):
-                raise TypeError("matrix must be a matrix of integers/floats")
+                raise TypeError(typeError)
 
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
@@ -25,8 +29,7 @@ def matrix_divided(matrix, div):
     lon = len(matrix[0])
     for row in range(len(matrix)):
         if len(matrix[row]) != lon:
-            raise TypeError("Each row of the matrix must be\
-                    have the same size")
+            raise TypeError("Each row of the matrix must have the same size")
     for j in matrix:
         new_matrix.append(list(map(lambda c: round(c / div, 2), j)))
     return new_matrix
