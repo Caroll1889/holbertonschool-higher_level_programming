8dff5319147c8ea078ce038f2a9e0ca542466cff
Caroll1889 <diahancaroll@hotmail.com>
2019-10-06 00:21:43 +0000

    text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        print(text[i], end="")
+
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print()
+            print()
