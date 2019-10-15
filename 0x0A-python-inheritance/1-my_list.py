e21f509cddc69f4439001a55ebd51ab1938551ba
Caroll1889 <diahancaroll@hotmail.com>
2019-10-15 20:02:46 +0000

    added docstring

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,8 +2,10 @@
 
 
 class MyList(list):
+    """class that inherits from list"""
     def __init__(self):
         super().__init__(self)
-
+   
+   """print a list"""
     def print_sorted(self):
         print(sorted(self))
