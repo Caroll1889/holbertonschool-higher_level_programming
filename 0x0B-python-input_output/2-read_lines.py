2e7c4856a30e2cf1093596446074711309f63464
Caroll1889 <diahancaroll@hotmail.com>
2019-10-16 22:00:54 +0000

    read lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding='utf-8') as file:
+
+        if nb_lines > 0:
+            for i in range(nb_lines):
+                print(file.readline(), end="")
+        else:
+            print(file.read())
