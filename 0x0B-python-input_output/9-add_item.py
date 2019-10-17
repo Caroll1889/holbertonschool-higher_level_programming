3568f885bf536af821df2e43451d1b875042daef
Caroll1889 <diahancaroll@hotmail.com>
2019-10-17 23:47:31 +0000

    fixed save

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -11,8 +11,8 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 try:
     lista = load_from_json_file('add_item.json')
 except:
-    lista = list()
+    lista = []
 
 for i in range(1, len(sys.argv)):
     lista.append(sys.argv[i])
-save_to_json_file('add_item.json', lista)
+save_to_json_file(lista, 'add_item.json')
