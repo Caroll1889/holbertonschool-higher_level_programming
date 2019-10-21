19bdf9beef0a2574b302dbe0ce2d430034534451
Caroll1889 <diahancaroll@hotmail.com>
2019-10-21 23:00:08 +0000

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""class inherits from rectangle"""
+
+
+class Square(Rectangle):
+    """constructor """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Inheriting from rectangle"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                  self.y, self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        for i, arg in enumerate(args):
+            if i == 0:
+                self.id = arg
+            if i == 1:
+                self.size = arg
+            if i == 2:
+                self.x = arg
+            if i == 3:
+                self.y = arg
+
+        if not args:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """dictionary representation of a square"""
+        new_dic = {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        return new_dic
