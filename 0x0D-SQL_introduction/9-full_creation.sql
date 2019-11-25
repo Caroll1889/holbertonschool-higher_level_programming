96f361759f4dc5738cf8f44c20c2d8f8eb6392f0
Caroll1889 <diahancaroll@hotmail.com>
2019-11-25 20:48:23 +0000

    creates another table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a table and add multiple rows
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
@@ -9,4 +9,4 @@ INSERT INTO second_table(id, name, score)
 VALUES (1, 'John', 10),
        (2, 'Alex', 3),
        (3, 'Bob', 14),
-       (4, 'George',8);
+       (4, 'George', 8);
