5c05874c93b7a2248d5ceb689998cfddc16b4a50
Caroll1889 <diahancaroll@hotmail.com>
2019-12-15 22:41:28 +0000

    add conditional

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     res = cur.fetchall()
 
     for row in res:
-        print(row)
+        if row [1][0] == 'N':
+            print(row)
     cur.close()    
     db.close()
