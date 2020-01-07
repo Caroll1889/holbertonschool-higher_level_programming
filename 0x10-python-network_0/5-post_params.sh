38c67c346199272840520cafd5a61eb0b9ec9d10
Caroll1889 <diahancaroll@hotmail.com>
2020-01-07 02:30:11 +0000

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, and displays the body of the response
-curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"/post
+curl -sXd POST "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
