d373eafb2616989fa2c7fdbddc5a6894ea878a34
Caroll1889 <diahancaroll@hotmail.com>
2020-01-13 23:25:23 +0000

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-# script that takes in a URL and an email, sends a POST request to the 
-# passed URL with the email as a parameter, and displays the body 
+# script that takes in a URL and an email, sends a POST request to the
+# passed URL with the email as a parameter, and displays the body
 # of the response
 
 from urllib.request import urlopen
 import sys
-#from urllib.parse import urlencode
 import urllib.parse
 import urllib.request
 
@@ -13,11 +12,11 @@ import urllib.request
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    val = {'email' : sys.argv[2]}
+    val = {'email': sys.argv[2]}
 
     data = urllib.parse.urlencode(val)
     data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
     with urlopen(req) as resp:
-       html = resp.read()
-    print(html.decode()) 
+        html = resp.read()
+    print(html.decode())
