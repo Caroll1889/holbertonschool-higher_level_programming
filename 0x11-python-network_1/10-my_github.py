5808d4438010c669cfe5bf6d44e242dd9ca522c2
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 04:45:39 +0000

    My github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# My github
+
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    ps_wd = sys.argv[2]
+
+    g_hub = requests.get('https://api.github.com/user',
+                         auth=HTTPBasicAuth('user', 'pass'))
+    try:
+        print(g_hub.json()['id'])
+
+    except Exception:
+        pass
