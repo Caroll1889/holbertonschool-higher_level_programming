6db0ea05d267651b54a26ba45bf5817b37963194
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 04:09:07 +0000

    search an API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Search API
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = 'http://0.0.0.0:5000/search_user'
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    response = requests.post(url, data={'q': q})
+    try:    
+        json_response = response.json()
+        if len(json_response) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(json_response.get('id'), json_response.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
