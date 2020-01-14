67ee929259672c22f5933f66fe0994c86bf409dc
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 00:35:27 +0000

    What's my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+
+import requests
+
+if __name__ == "__main__":
+    resp = requests.get('https://intranet.hbtn.io/status')
+    
+    print('Body response:')
+    print('\t- type:', (type(resp.text)))
+    print('\t- content:', resp.text)
