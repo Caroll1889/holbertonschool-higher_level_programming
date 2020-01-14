4026375fb9ebb2d926c9747246f00a2b71be9f52
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 04:55:11 +0000

    My github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     ps_wd = sys.argv[2]
 
     g_hub = requests.get('https://api.github.com/user',
-                         auth=('u_name', 'ps_wd'))
+                         auth=(u_name, ps_wd))
     try:
         print(g_hub.json()['id'])
 
     except Exception:
-        pass
+        print('None')
