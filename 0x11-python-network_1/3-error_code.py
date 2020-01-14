05a9c0d1e0f90825411000966500f06db92d453b
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 00:14:24 +0000

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,12 +4,13 @@
 
 from urllib.request import urlopen
 import sys
+import urllib.error
 
 if __name__ == "__main__":
 
     try:
         with urlopen(sys.argv[1]) as resp:
             html = resp.read()
-        print(html.decode("utf-8"))
+        print(html.decode())
     except urllib.error.HTTPError as err:
         print('Error code: {}'.format(err.code))
