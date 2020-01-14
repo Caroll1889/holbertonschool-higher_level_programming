8582e47316ba7f532b230ce689c4488dce9c1741
Caroll1889 <diahancaroll@hotmail.com>
2020-01-14 04:13:56 +0000

    search an API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         q = sys.argv[1]
     response = requests.post(url, data={'q': q})
-    try:    
+    try:
         json_response = response.json()
         if len(json_response) == 0:
             print('No result')
         else:
-            print('[{}] {}'.format(json_response.get('id'), json_response.get('name')))
+            print('[{}] {}'.format(json_response.get('id'),
+                  json_response.get('name')))
     except ValueError:
         print('Not a valid JSON')
