b0ebec96d48ed60d001c51ce0004c17f6e2fd710
Caroll1889 <diahancaroll@hotmail.com>
2020-02-04 19:54:43 +0000

    fixed conditional

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const MyArgs = process.argv.slice(2);
 
 if (MyArgs < 1) {
   console.log('No argument');
-} else if (MyArgs === 1) {
+} else if (MyArgs.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
