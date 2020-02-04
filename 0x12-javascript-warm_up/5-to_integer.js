719080a0daec5976eceb2ab696cfc18d8b40bf33
Caroll1889 <diahancaroll@hotmail.com>
2020-02-04 21:54:39 +0000

    fixed semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const MyArgs = process.argv.slice(2);
 if (Math.floor(MyArgs[0])) {
   console.log(`My number: ${Math.floor(MyArgs[0])}`);
 } else {
-  console.log('Not a number')
+  console.log('Not a number');
 }
