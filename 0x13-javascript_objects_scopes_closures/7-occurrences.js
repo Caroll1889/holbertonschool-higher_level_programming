207d149c5941249b8e011fbcb3b232bffabf0f55
Caroll1889 <diahancaroll@hotmail.com>
2020-02-10 21:57:09 +0000

    occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let num = 0;
+
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      num = num + 1;
+    }
+  }
+  return num;
+};
