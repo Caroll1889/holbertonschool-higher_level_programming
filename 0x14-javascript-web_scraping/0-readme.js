b2a910557124fd066e5f9047aabf3eee45fb3a0a
Caroll1889 <diahancaroll@hotmail.com>
2020-02-11 15:31:22 +0000

    read and print

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const MyArgs = process.argv.slice(2);
+
+fs.readFile(MyArgs[0], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  } else {
+      console.log(data);
+  }
+});
