39ef195a5c58740c5fc684b1d1dae6bc9f18ba13
Caroll1889 <diahancaroll@hotmail.com>
2020-02-11 17:16:07 +0000

    write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const MyArgs = process.argv.slice(2);
+
+fs.writeFile(MyArgs[0], MyArgs[1], function(err) {
+  if (err) {
+    return console.log(err);
+  }
+});
