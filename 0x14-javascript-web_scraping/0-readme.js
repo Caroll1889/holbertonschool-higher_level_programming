685785fbe83c337dc1fe74d21ba09e9ca2e18dc4
Caroll1889 <diahancaroll@hotmail.com>
2020-02-11 17:34:44 +0000

    fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
 const MyArgs = process.argv.slice(2);
 
 fs.readFile(MyArgs[0], 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   } else {
-      console.log(data);
+    process.stdout.write(data);
   }
 });
