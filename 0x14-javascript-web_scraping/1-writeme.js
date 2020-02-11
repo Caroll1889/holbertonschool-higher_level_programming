c26964c4b7b96aeb3643af4dc72df798ba117a72
Caroll1889 <diahancaroll@hotmail.com>
2020-02-11 17:23:28 +0000

    fixed utf8

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const MyArgs = process.argv.slice(2);
 
-fs.writeFile(MyArgs[0], MyArgs[1], function(err) {
+fs.writeFile(MyArgs[0], MyArgs[1], 'utf8', function (err) {
   if (err) {
     return console.log(err);
   }
