e0c00da9a0d07d4f2b6919bf08e0e766d643a9f5
Caroll1889 <diahancaroll@hotmail.com>
2020-02-12 00:31:49 +0000

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const MyArgv = process.argv.slice(2);
+
+request(MyArgv[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(MyArgv[1], 'utf8', body);
+  }
+});
