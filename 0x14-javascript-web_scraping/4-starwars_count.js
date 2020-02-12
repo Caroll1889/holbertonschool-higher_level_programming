67cd30696adb013f56fcd2043aaf97ec525398c5
Caroll1889 <diahancaroll@hotmail.com>
2020-02-12 22:30:31 +0000

    films

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv.slice(2);
+let num = 0;
+
+request(url[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  for (const res of data.results) {
+    for (const chars of res.characters) {
+      if (chars.indexOf('people/18/') !== -1) {
+        num = num + 1;
+      } else {
+      }
+    }
+  }
+  console.log(num);
+});
