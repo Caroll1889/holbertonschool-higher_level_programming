438ab3f3178889097ef632ff85396c1526f84cbb
Caroll1889 <diahancaroll@hotmail.com>
2020-02-12 23:04:24 +0000

    films

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 const url = process.argv.slice(2);
 let num = 0;
 
-request(url[0], function (err, response, body) {
+request(url[0], (err, response, body) => {
   if (err) {
     console.log(err);
   }
