56c31e306aff787ae6c047c23a9f0c74169e2b2c
Caroll1889 <diahancaroll@hotmail.com>
2020-02-13 21:36:01 +0000

    fixed semistandart

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 $(function () {
-  $("#red_header").click(function() {
-    $("HEADER").css("color", "#FF0000");
-  })
+  $('#red_header').click(function () {
+    $('HEADER').css('color', '#FF0000');
+  });
 });
